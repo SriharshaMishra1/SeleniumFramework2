ace39412420f4c2158f1b1bea3ca530a8b378c51
sriharsha <sriharsha@sriharsha-PC>
2018-12-28 01:02:57 +0530

    first time-Add Project

---
+++ selenium_new/src/ObjectReposetory/verifyingOrganisationPage.java
@@ -0,0 +1,25 @@
+package ObjectReposetory;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.vtiger.genericlib.BaseClass;
+
+public class verifyingOrganisationPage extends BaseClass {
+	
+	//initialization (Stores all WebElement Using @findBy)
+	
+	@FindBy(xpath="//span[contains(text(),'Organization Information')]")
+	private WebElement verifyingOrg;
+	
+	public void  verifyingOrg()
+	{
+		boolean b2= verifyingOrg.isDisplayed();
+		assertTrue(b2);
+				
+	}
+	
+
+}
